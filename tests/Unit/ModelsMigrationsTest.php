afa4d3395190857982644d0ad77b57114c32c550
hussainali <hussainali9616@gmail.com>
2024-02-23 01:45:08 +0500

    added the following models with migrations also added tests for it: * Batch * BatchProduct * Employee * Material * MaterialProduct * Order * OrderItem * Supplier

--- tests/Unit/ModelsMigrationsTest.php
+++ tests/Unit/ModelsMigrationsTest.php
@@ -54,4 +54,88 @@ class ModelsMigrationsTest extends TestCase
 
         $this->assertNotEmpty($expenses_migration);
     }
+
+    public function test_supplier_model_exists(): void
+    {
+        $this->assertTrue(class_exists('App\Models\Supplier'));
+    }
+
+    public function test_suppliers_migration_exists(): void
+    {
+        $suppliers_migration = $this->getMigration('create_suppliers_table.php');
+
+        $this->assertNotEmpty($suppliers_migration);
+    }
+
+    public function test_employee_model_exists(): void
+    {
+        $this->assertTrue(class_exists('App\Models\Employee'));
+    }
+
+    public function test_employees_migration_exists(): void
+    {
+        $employees_migration = $this->getMigration('create_employees_table.php');
+
+        $this->assertNotEmpty($employees_migration);
+    }
+
+    public function test_material_model_exists(): void
+    {
+        $this->assertTrue(class_exists('App\Models\Material'));
+    }
+
+    public function test_materials_migration_exists(): void
+    {
+        $materials_migration = $this->getMigration('create_materials_table.php');
+
+        $this->assertNotEmpty($materials_migration);
+    }
+
+    public function test_batch_product_model_exists(): void
+    {
+        $this->assertTrue(class_exists('App\Models\BatchProduct'));
+    }
+
+    public function test_batch_products_migration_exists(): void
+    {
+        $product_batch_products_migration = $this->getMigration('create_batch_products_table.php');
+
+        $this->assertNotEmpty($product_batch_products_migration);
+    }
+
+    public function test_material_product_model_exists(): void
+    {
+        $this->assertTrue(class_exists('App\Models\MaterialProduct'));
+    }
+
+    public function test_material_products_migration_exists(): void
+    {
+        $material_products_migration = $this->getMigration('create_material_products_table.php');
+
+        $this->assertNotEmpty($material_products_migration);
+    }
+
+    public function test_order_model_exists(): void
+    {
+        $this->assertTrue(class_exists('App\Models\Order'));
+    }
+
+    public function test_orders_migration_exists(): void
+    {
+        $orders_migration = $this->getMigration('create_orders_table.php');
+
+        $this->assertNotEmpty($orders_migration);
+    }
+
+    public function test_order_item_model_exists(): void
+    {
+        $this->assertTrue(class_exists('App\Models\OrderItem'));
+    }
+
+    public function test_order_items_migration_exists(): void
+    {
+        $order_items_migration = $this->getMigration('create_order_items_table.php');
+
+        $this->assertNotEmpty($order_items_migration);
+    }
 }
